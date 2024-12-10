ff96ffc5d8c898f6ae6731b83ae5bf573af63753
Herick2D <contato.herickmoreira@gmail.com>
2024-12-10 18:01:48 -0300

    INIT: NearBy

---
+++ app/src/main/java/com/herick/nearby/ui/component/category/NearbyFilterChip.kt
@@ -0,0 +1,13 @@
+package com.herick.nearby.ui.component.category
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun NearByCategoryFilterChip(
+    modifier: Modifier = Modifier,
+    isSelected: Boolean,
+    onClick: (isSelect: Boolean) -> Unit,
+) {
+    
+}
